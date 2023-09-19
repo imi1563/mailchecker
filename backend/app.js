5cd0e4df5af16433dc1e9d24b37d4b839573f144
imi1563 <imran@senewtech.com>
2023-09-19 16:51:14 -0700

    orig

--- backend/app.js
+++ backend/app.js
@@ -21,18 +21,18 @@ const mongoConnection = async (params) => {
   }
 };
 mongoConnection();
-// app.use((req, res, next) => {
-//   //res.setHeader("Access-Control-Allow-Origin", "http://172.232.1.230");
-//   res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "http://143.42.205.96");
+  //res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
 
-//   res.setHeader(
-//     "Access-Control-Allow-Methods",
-//     "GET, POST, PUT, DELETE,PATCH "
-//   );
-//   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
-//   next();
-// });
-app.use(cors());
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE,PATCH "
+  );
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
+});
+//app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(express.static(__dirname + "/public"));
