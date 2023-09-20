17257778bf69acc536c74bbed048f48aaa8451c2
imi1563 <imran@senewtech.com>
2023-09-20 09:14:37 +0000

    some changes

--- backend/app.js
+++ backend/app.js
@@ -21,23 +21,23 @@ const mongoConnection = async (params) => {
   }
 };
 mongoConnection();
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "http://143.42.205.96");
-  //res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+// app.use((req, res, next) => {
+//   res.setHeader("Access-Control-Allow-Origin", "http://143.42.205.96");
+//   //res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
 
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET, POST, PUT, DELETE,PATCH "
-  );
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
-  next();
-});
-//app.use(cors());
+//   res.setHeader(
+//     "Access-Control-Allow-Methods",
+//     "GET, POST, PUT, DELETE,PATCH "
+//   );
+//   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+//   next();
+// });
+app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(express.static(__dirname + "/public"));
 app.use("/emails", emailRouter);
-app.use("/user", userRouter);
+app.use("/api/user", userRouter);
 
 const port = process.env.PORT || 2022;
 app.listen(port, () => {
